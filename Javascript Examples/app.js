6fbc1db118f737e198703179200ab74a0a781a60
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-02 12:12:43 -0400

    Welcome to Javascript

---
+++ Javascript Examples/app.js
@@ -0,0 +1,11 @@
+var cat = "cat"
+var myNumber //Variable declaration
+myNumber = 42 // Variable initialization
+console.log(cat)
+// Console is a Javascript browser object.
+// Log will print the argument to the screen.
+// cat is the argument
+/* The plus operator + has many usess.
+    1). Concatenation
+*/
+console.log("The number is " + myNumber) // Converts myNumber from number to string tempporarily.
