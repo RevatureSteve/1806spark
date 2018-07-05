0a073c78bfbf29bf9ea04f9e9e38966311ad3ded
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-05 09:32:51 -0400

    Update to JS Assignment

---
+++ Jasmine_Onwuzulike_Code/JS_Assignment/assignment.js
@@ -0,0 +1,17 @@
+// 1. Fibonacci
+// Define function: fib(n) 
+// Return the nth number in the fibonacci sequence.
+function fib(n) {
+
+}
+
+// 3. Reverse String
+// Define function: reverseStr(someStr)
+// Reverse and return the String.
+function reverseStr(someStr) {
+     var firstStr = " ";
+     for (var i = someStr.length - 1 ; i >= 0; i--){
+        firstStr += someStr[i];
+     }
+     return firstStr;
+}
