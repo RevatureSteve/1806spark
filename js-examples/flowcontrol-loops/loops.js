1278bc7920912d0060ac2322ff817599e35974e9
Your Name <you@example.com>
2018-07-05 15:58:07 -0400

    new files

--- js-examples/flowcontrol-loops/loops.js
+++ js-examples/flowcontrol-loops/loops.js
@@ -4,22 +4,26 @@
 
 var names = ['mary', 'tom', 'anne', 'bobbert'];
 
-function printNumberPlus1(num){
+function printNumberPlus1(num) {
+    console.log(num++);
+    console.log(num);
+
+    console.log(++num);
+}
+
+function printNumberMinus1(num) {
+    console.log(--num);
+    return num++;
+}
+
+function printArrayLength(arr) {
+    console.log("The length of your array is: " + arr.length);
+}
+
+function printPrettyArrays(arr) {
 
 }
 
-function printNumberMinus1(num){
-    
-}
-
-function printArrayLength(arr){
-
-}
-
-function printPrettyArrays(arr){
-
-}
-
-function removeElementFromArray(arr, element){
+function removeElementFromArray(arr, element) {
 
 }
