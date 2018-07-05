87c751761f5dc29089b5d64dbca7df20dcda3daf
Chinedu Ozodi <chin2413@gmail.com>
2018-07-05 12:35:26 -0400

    started js assignments

---
+++ chinedu_ozodi_code/assignments/js_assignment/domManipulation.js
@@ -0,0 +1,62 @@
+// 1. USA
+// Define function getUSA()
+// Find the html element that contains "USA".
+// Print that element's contents.
+
+function getUSA(){
+    var usaElement = document.getElementById("USA");
+    console.log(usaElement.innerHTML);
+}
+
+// 2. Sales
+// Define function getPeopleInSales()
+// Print the names of all the people in the sales department.
+
+function getPeopleInSales(){
+    var person = document.getElementsByClassName("empName");
+    // console.log(person);
+    //Determin if in Sales
+
+    for (var i = 0; i < person.length; i++){
+        // console.log(person[i].nextElementSibling);
+        if (person[i].nextElementSibling.innerHTML == "Sales"){
+            console.log(person[i].innerHTML);
+        }
+    }
+}
+
+// 3. Click Here
+// Define function getAnchorChildren()
+// Find all anchor elements with a <span> child.
+// Print the contents of <span>
+
+function getAnchorChildren(){
+    var anchorElements = document.getElementsByTagName("a");
+    for (var i = 0; i < anchorElements.length; i++){
+        // console.log(person[i].nextElementSibling);
+        var spanElements = anchorElements[i].getElementsByTagName("span");
+
+        for (var j = 0; j < spanElements.length; j++){
+            console.log(spanElements[j].innerHTML);
+        }
+    }
+}
+
+// 4. Hobbies
+// Define function getHobbies()
+// Find all checked options in the 'skills' select element.
+// Print the value and the contents.
+
+function getHobbies(){
+    var skillsElements = document.getElementsByName("skills");
+    for (var j = 0; j < skillsElements.length; j++){
+        var skillsElement = skillsElements[j];
+        for (var i = 0; i < skillsElement.options.length; i++){
+            var option = skillsElement.options[i];
+            // console.log(person[i].nextElementSibling);
+            if (option.selected == true){
+                console.log(option.value + " - " +option.innerHTML);
+            }
+        }
+    }
+}
---
+++ chinedu_ozodi_code/assignments/js_assignment/javascript_part_1.js
@@ -0,0 +1,78 @@
+
+/*
+1. Fibonacci
+Define function: fib(n) 
+Return the nth number in the fibonacci sequence.
+*/
+
+function fib(n) {
+    if (n < 0){
+
+        return undefined;
+    }
+    if (n == 0){
+
+        return 0;
+    }
+    if (n == 1){
+
+        return 1;
+    }
+    return n + fib(n - 1);
+}
+
+/*
+2. Bubble Sort
+Define function: bubbleSort(numArray)
+Use the bubble sort algorithm to sort the array.
+Return the sorted array.
+*/
+
+function bubbleSort(numArray) {
+    //Create a copy of the array
+    var sortedArray = numArray.slice();
+    var arrayLength = sortedArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+        //Number of passes = length of array;
+        for (var j = 0; j < arrayLength - i - 1; j++) {
+            //Each pass is one less than the previous pass, because the last number is sorted
+            if (sortedArray[j] > sortedArray[j + 1]) {
+                //Switch numbers
+                var numHolder = sortedArray[j];
+                sortedArray[j] = sortedArray[j + 1];
+                sortedArray[j + 1] = numHolder;
+            }
+        }
+    }
+    return sortedArray;
+}
+
+/*
+3. Reverse String
+Define function: reverseStr(someStr)
+Reverse and return the String.
+
+*/
+
+function reverseStr(someStr){
+    var stringArray = someStr.split("");
+    var reverseArray = stringArray.reverse();
+    var reverseStr = reverseArray.join();
+    return reverseStr;
+}
+
+/*
+4. Factorial
+Define function: factorial(someNum)
+Use recursion to compute and return the factorial of someNum.
+*/
+
+function factorial(someNum){
+    if (someNum < 1){
+        return 0;
+    }
+    if (someNum = 1){
+        return 1;
+    }
+    return someNum * factorial(someNum - 1);
+}
