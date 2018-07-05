a66de40cee820511657a497d528c6a8e0a0e4451
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-05 10:13:55 -0400

    Updates to JS Assignment

--- Jasmine_Onwuzulike_Code/JS_Assignment/assignment.js
+++ Jasmine_Onwuzulike_Code/JS_Assignment/assignment.js
@@ -2,7 +2,9 @@
 // Define function: fib(n) 
 // Return the nth number in the fibonacci sequence.
 function fib(n) {
-
+var fibonacci  = [1,3,4,6,8,2,8,];
+var num = fibonacci[n];
+return num;
 }
 
 // 3. Reverse String
@@ -14,4 +16,22 @@ function reverseStr(someStr) {
         firstStr += someStr[i];
      }
      return firstStr;
+}
+
+// 9. Object literal
+// Define function traverseObject(someObj)
+// Print every property and it's value.
+function traverseObject(someObj) {
+
+}
+
+// 4. Factorial
+// Define function: factorial(someNum)
+// Use recursion to compute and return the factorial of someNum.
+function factorial(someNum) {
+    var numArray = [];
+    for (var i = someNum; i >= 0; i--) {
+    numArray.push(i);
+    }
+    return numArray;
 }
