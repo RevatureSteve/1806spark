15688f5d9dda27d02f76c6b3f982cea9ba38848b
Tyler <tyler.d.atkinson@gmail.com>
2018-07-05 13:56:27 -0400

    added new java practice file

---
+++ java_practice/javaPractice.js
@@ -0,0 +1,26 @@
+// document.getElementById("demo").innerHTML = "My First Javascript";
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed";
+// }
+
+// document.getElementById("demo").innerHTML = 5 + 6;
+
+// window.alert( 100 + 100);
+
+// console.log ("I am learning Java");
+
+// console.log (5 + 6);
+
+// var x, y, z;
+// x = 5;
+// y = 6;
+// z = x + y;
+
+// document.getElementById("demo").innerHTML = "The value of z is " + z + ".";
+
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+
+document.getElementById("demo").innerHTML = "The total is: " + total;
