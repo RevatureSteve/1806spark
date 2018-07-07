ef4784657aec97547dc70491849271a91cc0d5e0
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-07 11:27:01 -0400

    New examples

--- js-examples/flowcontrol-loops/loops.js
+++ js-examples/flowcontrol-loops/loops.js
@@ -1,3 +1,6 @@
+console.log("Ready for work");
+
+
 /*
     Loops in Javascript
 */
@@ -5,21 +8,25 @@
 var names = ['mary', 'tom', 'anne', 'bobbert'];
 
 function printNumberPlus1(num){
+console.log(++ num); 
 
 }
 
 function printNumberMinus1(num){
-    
+    console.log(--num);
 }
 
 function printArrayLength(arr){
-
+    console.log("The length of your array is " + arr.length);
 }
 
 function printPrettyArrays(arr){
-
+for (var i = 0; i < arr.length; i++) {
+    console.log("The index is " + i + "and the value is " + arr.value);
+}
 }
 
 function removeElementFromArray(arr, element){
-
+    console.log("Hi!");
+    console.log(arr.pop);
 }
