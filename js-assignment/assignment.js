fa90d8637c083d2d2b8e6dbeb76043d9eb819e7d
Obosa Nosa-Igiebor <obosanosa-igiebor@Obosas-MacBook-Pro.local>
2018-07-08 19:54:34 -0400

    I am pushing my assignment

---
+++ js-assignment/assignment.js
@@ -0,0 +1,52 @@
+/* Fibonacci
+Define function: fib(n) 
+Return the nth number in the fibonacci sequence.
+*/
+
+function fib(n) {
+    if (n <= 1) return 1;
+  
+    return fib(n - 1) + fib(n - 2);
+  }
+/*
+ Delete Element
+Define function deleteElement(someArr)
+Print length
+Delete the third element in the array.
+Print length
+The lengths should be the same
+*/
+  someArr = ['mary', 'tom', 'anne', 'bobbert'];
+  function deleteElement(someArr){
+      someArr.length;
+      delete someArr[2];
+  }
+/*
+Splice Element
+Define function spliceElement(someArr)
+Print length
+Splice the third element in the array.
+Print length
+The lengths should be one less than the original length
+*/
+
+someArr = ['mary', 'tom', 'anne', 'bobbert'];
+function spliceElement(someArr) {
+         someArr.length;
+         someArr.splice(2,1);
+         someArr.length;
+
+}
+/*
+Reverse String
+Define function: reverseStr(someStr)
+Reverse and return the String
+*/
+str = "RevatureSpark"
+function reverseStr(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
