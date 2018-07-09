507cafbb5f49fdc3f9fdcea911706ca58575f3c1
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-09 16:51:00 -0400

    Finished part one and some of part two

--- Jasmine_Onwuzulike_Code/Fizz_Buzz_JS_Assignment/fizzbuzz.js
+++ Jasmine_Onwuzulike_Code/Fizz_Buzz_JS_Assignment/fizzbuzz.js
@@ -1,5 +1,20 @@
-function printNums(event) {
-    var num = document.getElementById("in").innerHTML;
-            console.log(num);
 
+function getInput() {
+    var x = document.getElementById("in").value;
+    var list = document.getElementById("demo");
+    var count = [];
+    for (var i = 0; i <= x; i++) {
+    console.log(i);
+    list.innerHTML += i;
+    count.push(i); 
+    }
+    return count;
 }
+
+function clearAll() {
+   var x = document.getElementById("in").value = " ";
+   var y = document.getElementById("demo").innerHTML = " ";
+}
+
+
+
