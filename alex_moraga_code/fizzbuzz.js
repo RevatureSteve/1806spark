0b0869d32d77eea37f6fc70c3554874938e1d7b8
AlexMoraga <alex.moraga94@gmail.com>
2018-07-09 19:00:11 -0400

    final submission

--- alex_moraga_code/fizzbuzz.js
+++ alex_moraga_code/fizzbuzz.js
@@ -1,28 +1,28 @@
-document.addEventListener("change", fizzbuzz);
+document.addEventListener("input", fizzbuzz);
 
 function fizzbuzz() {
 
-    var N = document.getElementsByName("input")[0].value;
+    var N = document.getElementById("water").value;
     //document.getElementsByTagName("li")[0].innerHTML = list;
-    var listItem = document.createElement("li");
     var list = document.getElementById("unordered");
-    list.appendChild(listItem);
+
 
     for (i = 1; i <= N; i++) {
+        var listItem = document.createElement("li");
 
-        if (i % 3 == 0) {
-            document.createTextNode("fizz");
+        list.appendChild(listItem);
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            document.getElementsByTagName("li")[i - 1].innerHTML = "buzzfizz";
+        }
+        else if (i % 3 == 0) {
+            document.getElementsByTagName("li")[i - 1].innerHTML = "fizz";
         }
         else if (i % 5 == 0) {
-            document.createTextNode("buzz");
-            //buzz
-        }
-        else if (i % 3 == 0 && i % 5 == 0) {
-            document.createTextNode("fizzbuzz");
-            //fizzbuzz
+            document.getElementsByTagName("li")[i - 1].innerHTML = "buzz";
         }
         else {
-            document.createTextNode(i);//print out i/N value
+            document.getElementsByTagName("li")[i - 1].innerHTML = i;
         }
     }
 }
