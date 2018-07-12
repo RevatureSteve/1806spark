597dadd56f617d9fbc08c98f809d67d29f97f87a
Steven K <steven.kelsey@revature.com>
2018-07-11 22:59:05 -0400

    initial node express example

---
+++ tampa_gems/server.js
@@ -0,0 +1,68 @@
+
+var express = require('express');
+var app = express();
+var jsonParser = require('body-parser').json;
+var path = require('path');
+
+
+
+app.listen(3000, function(){ console.log("hello, node server w/ express has started!");})
+
+app.use(function(req,resp, next){
+    console.log('Server has received an HTTPRequest - 1st stage');
+    // console.dir(req); // log request to console but maybe we want to send this request to a database logging system?
+    // console.log(req.body);
+    // console.log("Base URL: " + req.baseUrl);
+    // console.log(req.ip);
+    console.log('The request\'s headers: ');
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+    next();
+})
+app.use(function(req,resp, next){
+    console.log('Handling the HTTPRequest - 2nd stage');
+    
+    next();
+})
+app.use('/diff', function(req,resp, next){
+    console.log('Handling the HTTPRequest send to diff - 2nd stage');
+    
+    next();
+})
+app.use('/diff/:id', function(req,resp, next){
+    console.log('Handling the HTTPRequest send to diff ' + 'id: ' + req.params.id + ' - 2nd stage');
+    
+    next();
+})
+
+// https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=harry+potter
+app.use('/query', function(req,resp, next){
+    console.log('The color is ' + req.query.color);
+    next();
+})
+
+var jsonCheck = function(req, resp, next){
+    if(req.body){
+        console.log("The color is " + req.body.color);
+    }else{
+        console.log("No body found");
+    }
+    next();
+}
+
+app.use('/param', jsonCheck);
+app.use('/param', jsonParser())
+app.use('/param', jsonCheck);
+
+app.use('/home', function(req, resp, next) {
+    // resp.send('index.html');
+    // resp.end('index.html');
+    // resp.write('index.html');
+    // resp.sendFile('/Users/steveace6/my_git_repos/1806spark/tampa_gems/index.html');
+    resp.sendFile(path.join(__dirname+'/pages/index.html'));
+    // resp.json({"response": "hello json"});
+})
+
+app.use('/css', express.static('css'));
+app.use('/pages', express.static('pages'))
