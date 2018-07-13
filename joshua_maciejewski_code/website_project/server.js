bfd8071dbf30618c511c55359d9ae39fe48009da
Joshua Maciejewski <maciejewskijoshua@gmail.com>
2018-07-13 03:07:28 -0400

    end of 7/12

--- joshua_maciejewski_code/website_project/server.js
+++ joshua_maciejewski_code/website_project/server.js
@@ -4,3 +4,33 @@ var app = express();
 app.listen(3000, function(){
     console.log('Your node server with express is ready')
 })
+
+//allow css static files to be found easily
+app.use('/css', express.static('css'));
+//allow html files
+app.use('/pages', express.static('pages'));
+
+//request comes thru hit this function
+app.use(function(req, resp, next){     //needs request object and response object
+    console.log('hey developer on the server, i received a request!')
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+    next(); // go to the next function
+})
+
+app.use(function(req, resp,next){     
+    console.log('I am now going to do something with the request')
+    next(); // go to the next function if there isn't one......then we are telling express we are done 
+})
+
+app.use("/hello", function(req, resp,next){     
+    console.log('request received - hello')
+    next(); 
+
+})
+
+app.use("/home", function(req, resp, next) {
+    resp.sendFile('C:\\Users\\Joshua\\my_git_repos\\1806spark\\joshua_maciejewski_code\\website_project\\index.html')
+    //next(); don't need as this function is sending response back not next function
+})
