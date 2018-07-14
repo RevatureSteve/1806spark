71e4d8f9ee3e0a66a06dacd3132c4c321d4cfc88
Tyler <tyler.d.atkinson@gmail.com>
2018-07-14 11:11:16 -0400

    connected to server and db....verify u and P ftn

--- tyler_project_one/server.js
+++ tyler_project_one/server.js
@@ -5,25 +5,53 @@ var getScore = require("./getScore-route");
 var giveScore = require("./giveScore-route");
 var jsonParser = require('body-parser');
 
+app.listen(portNumber, function () {
+    console.log("Project 1 - Express server is listening on port 3001")
+})
+
+app.use('/project1css', express.static('project1css'));
+app.use('/project1', express.static('project1'));
+
 app.use(jsonParser());
 
-app.listen(portNumber, function(){
-    console.log("Project 1 - Express server is listening on port 3001")
-});
+app.use(function (req, resp, next) {
+    console.log('hey developer on teh server, I recieved a request');
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+    next(); //go to next function
 
-// use mongoose JS package to connect to mongo db
+})
+app.use('/home', function (req, resp, next) { //add extra \\ so it doesn't read "escape \"//
+resp.sendFile('C:\\Users\\tyler\\my_git_repos\\1806spark\\tyler_project_one\\project1.html');
+})
 
-var mongoose = require("mongoose");    
-mongoose.connect("mongodb://localhost:27017/tdb"); //projectOne DB
-var db = mongoose.connection; //reference connection and allow use
+var verifyUandP = function (req, resp, next) {
+        if (req.body) {
+            console.log('The color is:' + req.body.color);
+        } else {
+            console.log('There is no');
+        }
+        next();
+    };
 
-//error handling
-db.on("error", function(err){
-    console.error("connection error:", err);
-});
-db.once("open", function(){
-    console.log("Database Connection Successful!!");
-});
+    // use mongoose JS package to connect to mongo db
 
-app.use("/scores", getScore)
-app.use("/scoreboard", giveScore)
+    var mongoose = require("mongoose");
+    mongoose.connect("mongodb://localhost:27017/tdb"); //projectOne DB
+    var db = mongoose.connection; //reference connection and allow use
+
+    //error handling
+    db.on("error", function (err) {
+        console.error("connection error:", err);
+    });
+    db.once("open", function () {
+        console.log("Database Connection Successful!!");
+    });
+
+    app.use("/verifyUandP", jsonParser());
+    app.use("/verifyUandP", verifyUandP);
+    app.use("/scores", getScore)
+    app.use("/scoreboard", giveScore)
+
+  
