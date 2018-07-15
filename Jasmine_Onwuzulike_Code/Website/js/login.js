4ab27da297c2f671be0904bd09ca73b54b57ad24
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-14 21:49:29 -0400

    Able to get to login function

--- Jasmine_Onwuzulike_Code/Website/js/login.js
+++ Jasmine_Onwuzulike_Code/Website/js/login.js
@@ -1,16 +1,19 @@
 let url = "http://localhost:3000/login";
 
-//Login form fields.
-let firstName = document.getElementById("p_first");
-let lastName = document.getElementById("p_last");
-let email = document.getElementById("p_email");
 //Add event listener to create button.
-document.getElementById("createAcc").addEventListener("click", function() {
-   console.log("You have started creating an account!")
-});
+document.getElementById("createAcc").addEventListener("click", getEntryData);
 
-let data = {
-   "firstName": firstName,
-   "lastName": lastName,
-   "email": email
+//Get login form fields.
+
+function getEntryData() {
+    let firstName = document.getElementById("p_first").value;
+    let lastName = document.getElementById("p_last").value;
+    let email = document.getElementById("p_email").value;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+
 };
+
+
+
--- Jasmine_Onwuzulike_Code/Website/server.js
+++ Jasmine_Onwuzulike_Code/Website/server.js
@@ -1,23 +1,29 @@
 var express = require('express');
 var app = express();
-var loginPath = require("./login-paths");
+
+var options = {
+	dotfiles: 'ignore',
+	etag: false,
+	extensions: ['htm', 'html'],
+	index: false,
+	maxAge: '1d',
+	redirect: false,
+	setHeaders: function (res, path, stat) {
+	  res.set('x-timestamp', Date.now());
+	  res.status = 200;
+	}
+  }
+app.use("/css", express.static('css'));
+app.use("/pages", express.static('pages', options));
+app.use("/js", express.static('js'));
 
 
 
-// //Static pages.
- app.use("/css", express.static('css'));
-app.use('/', express.static(__dirname + '/pages'));
-app.use('/', express.static(__dirname + '/js'));
-
-
-
-//Set home page.
+// //Set home page.
 app.get('/home', function(req, res, next) {
     res.sendFile('C:\\Users\\Jasmine C Onwuzulike\\My_Git_Repos\\1806spark\\Jasmine_Onwuzulike_Code\\Website\\home.html');
 });
 
-//Use photography route.
-app.use('/login', loginPath);
 
 
 app.listen('3000', function() {
