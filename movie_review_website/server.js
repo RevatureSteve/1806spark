781d58525ba6c74c2413ce8049f958b8ae30a9d5
MarcinSalamon <salamonmarcin29@gmail.com>
2018-07-15 22:40:04 -0400

    need to pull new code from my laptop

--- movie_review_website/server.js
+++ movie_review_website/server.js
@@ -9,13 +9,14 @@ app.use(jsonParser());
 	connection to Database
 	Mongoose is the JavaScript Package used to talk to the mongo database
 */
+
 var mongoose = require("mongoose");  // require the package from node_modules
 mongoose.connect("mongodb://localhost:27017/reviews"); // sets the configuration the specific database called qa
 var db = mongoose.connection; // a reference to the connection to allow us to use the connection
-db.on("error", function(err){	// call this function on errors 
+db.on("error", function (err) {	// call this function on errors 
 	console.error("connection error:", err);
 });
-db.once("open", function(){ // creates connections when requests
+db.once("open", function () { // creates connections when requests
 	console.log("db connection successful");
 });
 
@@ -29,24 +30,24 @@ app.use('/js', express.static('js'));
 app.use('/images', express.static('images'));
 
 app.use(function (req, resp, next) {
-    console.log('request received');
-    console.log(req.headers);
-    console.log(req.method);
-    console.log(req.url);
-    next(); // go to the the nest function 
+	console.log('request received');
+	console.log(req.headers);
+	console.log(req.method);
+	console.log(req.url);
+	next(); // go to the the nest function 
 })
 
 app.use(function (req, resp, next) {
-    console.log('request processing');
-    next();
-    // then we are tellng express we are done
+	console.log('request processing');
+	next();
+	// then we are tellng express we are done
 })
 
 app.use("/home", function (req, resp, next) {
 
-    resp.sendFile('C:\\Users\\marci\\my_git_repos\\1806spark\\movie_review_website\\index.html');
-    //resp.sendfile('') // add javascript files
-    //next(): dont need this because as this function is sending the response
+	resp.sendFile('C:\\Users\\marci\\my_git_repos\\1806spark\\movie_review_website\\index.html');
+	//resp.sendfile('') // add javascript files
+	//next(): dont need this because as this function is sending the response
 
 
 })
@@ -54,13 +55,13 @@ app.use("/home", function (req, resp, next) {
 app.use("/reviews", reviewRoutes);
 app.use("/comments", commentRoutes);
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	var err = new Error("Not Found");
 	err.status = 404;
 	next(err);
 });
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
 	res.status(err.status || 500);  // set the response status to err.status but if falsy set to 500
 	res.json({
 		error: {
@@ -70,5 +71,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(3000, function () {
-    console.log('Movie server is up');
+	console.log('Movie server is up');
 });
