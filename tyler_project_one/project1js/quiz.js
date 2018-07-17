fd8f72e6db8203ca74221a7e0e9d6eef50274ae4
Tyler <tyler.d.atkinson@gmail.com>
2018-07-17 19:40:10 -0400

    tried to troubleshoot quiz issues

--- tyler_project_one/project1js/quiz.js
+++ tyler_project_one/project1js/quiz.js
@@ -133,6 +133,223 @@ function generateQuiz() {
         var answerContainers = quizContainer.querySelectorAll('.answers');
         // track users answers
         var userAnswer = '';
+        let numCorrect = 0;
+
+        // for each question
+
+        for (var i = 0; i < questions.length; i++) {
+           userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+               
+           if(userAnswer===questions[i].correctAnswer){
+           // add to number of correct answers
+                console.log(numCorrect);
+                numCorrect++;
+            }else {
+                numCorrect--;
+            }
+ 
+        }
+        // show number correct out of total
+        resultsContainer.innerHTML = numCorrect + 'out of' + myQuestions.length;
+
+    }
+
+    showQuestions();
+
+    submitButton.onclick = function () {
+        showResults();
+    }
+
+}
+
+
+function submitScoreFunction(){
+
+
+let url= 'http://localhost:3001/quiz';
+document.getElementById('results');
+
+let data = {
+    "score": numCorrect + 'out of' + questions.length,
+    
+}
+
+fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data), 
+    headers:{
+      'Content-Type': 'application/json'
+    }
+}).then(data=> data.json()).then(q => {
+        console.log(q);
+    
+});
+
+
+}
+
+var trackedScores = null;
+
+callScores();
+
+function callScores(){
+    let url= 'http://localhost:3001/quiz';
+
+    fetch(url).then((resp) => {
+        return resp.json();
+    }).then((data)=> {
+        console.log(data);
+        trackedScores = data;
+        showScores(trackedScores);
+    })
+}
+
+function showScores(s) {
+    console.log('set scores to page');
+    console.log(s);
+    for (i = 0; i < s.length; i++) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(s[i].score));
+        document.getElementById("scoreList").appendChild(li);
+    }
+}
+
+
+var quizContainer;
+var resultsContainer;
+var submitButton;
+
+window.onload = function () {
+    quizContainer = document.getElementById('quiz');
+    resultsContainer = document.getElementById('results');
+    submitButton = document.getElementById('submit');
+
+
+}
+
+var questions = [
+    {
+        question: "Select the Square",
+        answers: {
+            a: "circle.png",
+            b: "square.png",
+            c: "triangle.png"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Select the Triangle",
+        answers: {
+            a: "triangle.png",
+            b: "circle.png",
+            c: "square.png"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Select the Star",
+        answers: {
+            a: "star.png",
+            b: "square.png",
+            c: "circle.png"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Select the One",
+        answers: {
+            a: "zero.png",
+            b: "one.png",
+            c: "nine.png"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Select the Nine",
+        answers: {
+            a: "nine.png",
+            b: "one.png",
+            c: "five.png"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Select the Five",
+        answers: {
+            a: "five.png",
+            b: "nine.png",
+            c: "zero.png"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Select the BLUE Fish",
+        answers: {
+            a: "redfish.png",
+            b: "greenfish.png",
+            c: "bluefish.png"
+        },
+        correctAnswer: 'c'
+    },    {
+        question: "Select the GREEN fish",
+        answers: {
+            a: "greenfish.png",
+            b: "bluefish.png",
+            c: "yellowfish.png"
+        },
+        correctAnswer: 'a'
+    },    {
+        question: "Select the YELLOW fish",
+        answers: {
+            a: "redfish.png",
+            b: "yellowfish.png",
+            c: "bluefish.png"
+        },
+        correctAnswer: 'b'
+    },
+];
+var numCorrect;
+function generateQuiz() {
+
+
+
+    function showQuestions() {
+        // store quiz output and answer choices
+        var output = [];
+        var answers;
+
+        for (var i = 0; i < questions.length; i++) {
+            // reset the list of answers
+            answers = [];
+
+            for (letter in questions[i].answers) {
+                // add radio button
+                answers.push(
+                    '<label>'
+                    + '<input type = "radio" name = "question' + i + '" value= " ' + letter + '">'
+                    + letter + ': '
+                    + '<img src = "'+questions[i].answers[letter]+'">'
+                    + '</label>'
+                );
+            }
+            // add this question and answers to the output
+
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class= "answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // output list to string and display on page
+
+        quizContainer.innerHTML = output.join('');
+    }
+    
+    function showResults() {
+        // gather answer containers
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        // track users answers
+        var userAnswer = '';
         numCorrect = 0;
 
         // for each question
@@ -207,5 +424,4 @@ function showScores(s) {
         document.getElementById("scoreList").appendChild(li);
     }
 }
-
 
--- tyler_project_one/project1js/shapes.js
+++ tyler_project_one/project1js/shapes.js
@@ -1,12 +1,21 @@
+window.onload = function () {
+    document.getElementById("squareImage").onclick = myFunction;
+    document.getElementById("circleImage").onclick = myFunction;
+    document.getElementById("crescentImage").onclick = myFunction;
+    document.getElementById("hexagonImage").onclick = myFunction;
+}
+
 function myFunction() {
-    var popup = document.getElementById("myPopup");
+    let imageId = this.id;
+    console.log(this.id);
+    var popup = document.getElementById(imageId + "Popup");
     popup.classList.toggle("show");
-    
-    if (popup.paused){ 
-        popup.play(); 
-        }
-      else{ 
-        popup.pause();
-        }
-     
+
+    if (popup.paused) {
+        popup.play();
     }
+    else {
+        popup.pause();
+    }
+
+}
