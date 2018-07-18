ced3f29c73234dfe05e54c2f4d3d864de4d08fe3
jonwuzulike <jasonwuzulike@gmail.com>
2018-07-17 21:27:39 -0400

    Added admin page

---
+++ Jasmine_Onwuzulike_Code/Website/js/admin.js
@@ -0,0 +1,24 @@
+let adminUrl = "http://localhost:3000/login";
+var nameList = null;
+function setUp() {
+    fetch(adminUrl)
+    .then(function(res) {
+        res.json()
+    })
+    .then(function(names) {
+        console.log(names);
+        nameList = names;
+        setNames(nameList);
+    })
+
+    }
+
+    function setNames(nl) {
+        console.log(nl);
+        var nameSpace = document.getElementById('registered');
+        for (let i = 0; i < nl.length; i++) {
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = nl.first_name + " " + nl.last_name;
+            nameSpace.appendChild(newDiv);
+        }
+    }
--- Jasmine_Onwuzulike_Code/Website/js/login.js
+++ Jasmine_Onwuzulike_Code/Website/js/login.js
@@ -1,9 +1,7 @@
 
 let login = "http://localhost:3000/login";
 window.onload = function() {
-   document.getElementById('login-section').addEventListener("docus", function() {
-       document.getElementById('first-requirements').style.visibility = "visible";
-   })
+   
 
     //Add event listener to create button.
     document.getElementById("createAcc").addEventListener('click', getEntryData);
@@ -14,14 +12,6 @@ function getEntryData() {
    let firstName = document.getElementById('p_first').value;
    let lastName = document.getElementById('p_last').value;
    let email =  document.getElementById('p_email').value;
-   var string = "@";
-    if (email.includes(string)) {
-        document.getElementById('p_email').style.color = "green";
-    }else {
-        document.getElementById('p_email').style.color = "red";
-        alert("Please enter a valid email.");
-        return false;
-    }
    console.log(firstName + lastName + email);
 
 //POST Object 
@@ -43,7 +33,7 @@ fetch(login, {
 })
 .then(function(p) {
     console.log(p.first_name);
-    alert(`Thank you for signing up ${firstName}!!!! An email will be sent to ${email} shorty.`)
+    alert(`Thank you for signing up ${firstName}!!!! An email will be sent to ${email} shorty.`);
 })
 
 };
