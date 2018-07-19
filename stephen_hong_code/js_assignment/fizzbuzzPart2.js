752c276203d8c4e9757f72852aeb2a48bcc9a9f5
Stephen <stephenh.email@gmail.com>
2018-07-19 14:40:20 -0400

    fizzbuzz part 2 & 3

---
+++ stephen_hong_code/js_assignment/fizzbuzzPart2.js
@@ -0,0 +1,34 @@
+function fizzBuzz() {
+    var a = document.getElementById("someNum").value;
+    // Assign entered value to variable a
+    var b = document.getElementById("someNum2").value;
+    // Assign entered value to variable b
+    var c = document.createElement("UL");
+    c.setAttribute("id", "list");
+    document.body.appendChild(c);
+    // Create a ul element with id = "list" and append it as the last child of body element
+    var a = parseInt(a); //parses a string and returns an integer
+    var b = parseInt(b);
+
+    for (a; a <= b; a++) {
+        var d = document.createElement("LI");
+        // Create li for variable a amount of times
+        if (a%3 == 0 && a%15 !== 0) {
+            var e = document.createTextNode("Fizz");
+            // For each multiple of 3 but not multiple of 15, display "Fizz" instead of the number
+        } else if (a%5 == 0 && a%15 !== 0) {
+            var e = document.createTextNode("Buzz");
+            // For each multiple of 5 but not multiple of 15, display "Buzz" instead of the number
+        } else if (a%15 == 0) {
+            var e = document.createTextNode("FizzBuzz");
+            // For each multiple of 15, display "FizzBuzz" instead of the number
+        } else {
+            var e = document.createTextNode(a);
+            // Otherwise, display number
+        }
+        d.appendChild(e);
+        // Append text node as the last child of li
+        document.getElementById("list").appendChild(d);
+        // Append li as the last child of ul with id = "list"
+    }
+}
