3b3c7c033fbae45e8b7844a156bb0dffed2ddf8a
MarcinSalamon <Marcin+salamonmarcin29@gmail.com>
2018-07-20 09:55:02 -0400

    fizzbuzz part 2 and 3

--- marcin_salamon_code/fizzbuzz/fizzbuzz2.js
+++ marcin_salamon_code/fizzbuzz/fizzbuzz2.js
@@ -1,17 +1,26 @@
 var unordered;
 
 window.addEventListener('load', function () {
-    document.getElementById('btn1').onclick = fizzbuzz;
+    document.getElementById('btn2').onclick = fizzbuzz2;
     unordered = document.getElementsByTagName('ul')[0];
 });
 
-function fizzbuzz() {
+function fizzbuzz2() {
     while (unordered.firstChild) {
         unordered.removeChild(unordered.firstChild);
     }
 
-    var n = document.getElementById('n1').value;
-    for (var i = 1; i <= n; i++) {
+    let n1 = parseInt(document.getElementById('n1').value);
+    let n2 = parseInt(document.getElementById('n2').value);
+
+    if (n1 > n2) {
+        let temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+
+    for (var i = n1; i <= n2; i++) {
 
         var listItem = document.createElement('li');
         var displayStr = "";
