240273575894b85630e80c42a12be5a63b184fd8
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-07-24 11:38:36 -0400

    java update with for loop

--- Intro-Java/CoreJava/src/com/ex/classes/Launcher.java
+++ Intro-Java/CoreJava/src/com/ex/classes/Launcher.java
@@ -4,28 +4,31 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		Dog sparky = new Dog("sparky", 48, false, "zombie");
-		System.out.println(sparky.getName());
+		//System.out.println(sparky.getName());
 		
 		Fish nemo = new Fish("nemo", 13, true, 1);
-		System.out.println(nemo.getName());
+		//System.out.println(nemo.getName());
 		
 		Dog skippy = new Dog("skippy", 5, true, "husky");
-		System.out.println(skippy.getName());
+		//System.out.println(skippy.getName());
 		
 		Bird tucan = new Bird("tucan", 6, true);
-		System.out.println(tucan.getName());
+		//System.out.println(tucan.getName());
 		
 		Fish carl = new Fish("carl", 3, true, 1);
-		System.out.println(carl.getName());
+		//System.out.println(carl.getName());
 		
-//		Pet[] pets = new Pet[5];
-//		pet[0] = sparky;
-//		pet[1] = nemo;
-//		pet[2] = skippy;
-//		pet[3] = tucan;
-//		pet[4] = carl;
-//		
-//		for()
+		Pet[] pet = new Pet[5];
+		pet[0] = sparky;
+		pet[1] = nemo;
+		pet[2] = skippy;
+		pet[3] = tucan;
+		pet[4] = carl;
+		
+		//advanced FOR loop syntax
+		for(Pet p: pet) {
+			System.out.println(p.getName());
+		}
 	}
 	
 		
