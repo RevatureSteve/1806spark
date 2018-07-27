a8dbabc32146a254615df8e95ef20246947cc9fd
Steven Kelsey <steven.kelsey@revature.com>
2018-07-27 14:39:40 -0400

    Serialization

---
+++ Intro-Java/SerializationAndThreads/src/com/revature/Driver.java
@@ -0,0 +1,95 @@
+package com.revature;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.revature.beans.Student;
+import com.revature.dao.StudentDao;
+import com.revature.presentation.PresentationUtil;
+
+public class Driver {
+
+
+
+		
+	/*
+	 * 	Serialization: Java Objects to byte code
+	 * 	Deserialization: byte code to Java Objects
+	 * 
+	 * 	First note: All Java Objects are blacklisted, meaning you cannot do serialization
+	 * 					You must whitelist each individual class so the object created from
+	 * 					them are allowed to be serialized
+	 * 
+	 * 			*Serializable is a "Marker Interface"*
+	 * 			Marker Interfaces tells the JVM something special
+	 * 			Marker Interfaces have no methods!
+	 * 			The something special about Serializable is the whitelisting
+	 * 		1. The class must implements Serializable (whitelist)
+	 * 		2. Add a serialVersionUID to the class (OPTIONAL but best practice)
+	 * 		3. Use the ObjectOutStream or ObjectInputStream
+	 * 
+	 * 
+	 * 		What is the point of the serialVersionUID?
+	 * 			to match the version when deserializing
+	 * 		If the serialVersionUID doesn't match when deserializing what happens?
+	 * 			Throws an exception...but which one? InvalidClassException
+	 * 			
+	 */
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			PresentationUtil.welcomeMenu(); //we know welcomeMenu() is static because PresentationUtil is capital
+			int userInput = scan.nextInt();
+			System.out.println("User entered: " + userInput);
+			switch (userInput) {
+			case 1:
+				promptUserForNewStudentAndPersist();
+				break;
+			case 2:
+				getStudentAndDisplay();
+				break;
+			case 3:
+				System.out.println("exiting..");
+				System.exit(1);
+			}
+			
+		}
+//		serializeStudent(john, ); // Create of CRUD
+//		Student stud1 = StudentDao.deserializeStudent();	 // Read of CRUD
+		
+		
+	}
+	
+
+	public static void promptUserForNewStudentAndPersist() {
+		// First prompt user and retrieve input
+		Student stud = PresentationUtil.creatingNewStudent();
+		
+		// Persist Student to file
+		try {
+			StudentDao.serializeStudent(stud);
+		} catch (IOException e) {
+			PresentationUtil.errorView("File is corrupt");
+		}
+		
+	}
+	
+	
+	public static void getStudentAndDisplay() {
+		// retrieve student from StudentDAO (Data access object)
+		Student stud = StudentDao.deserializeStudent();
+			//Probably should of ducked the exceptions in the StudentDao and handled them here with try/catch
+		
+		PresentationUtil.presentStudent(stud);
+	}
+	
+}
+
+
+
+
+
+
+
---
+++ Intro-Java/SerializationAndThreads/src/com/revature/beans/Student.java
@@ -0,0 +1,99 @@
+package com.revature.beans;
+
+import java.io.Serializable;
+
+import com.revature.abstracts.Teachable;
+
+/*
+ * Student Class is not just a POJO
+ * Student Class is a JavaBean - stricter version of a POJO
+ * 
+ * 		1. state must be fully encapsulated
+ * 		2. provide getters and setter where needed
+ * 		3. must have a no-args constructor 
+ * 		4. class must implements Serializable
+ * 		5. override toString()
+ * 
+ */
+/**
+ * @author Steve Work PC
+ *
+ */
+public class Student implements Serializable, Teachable {
+	
+	/**
+	 * 	Used to deserialize and match the correct version that was used when serializing
+	 * 		(OPTIONAL but best practice)
+	 */
+	private static final long serialVersionUID = 102;
+	
+	private int id;
+	private String name;
+	private double gpa;
+	private int ssn;
+	
+	public Student() {}
+	
+	public Student(int id, String name, double gpa) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gpa = gpa;
+	}
+
+	// alt + shift + s then r for generating getters and setters
+
+	public Student(int id, String name, double gpa, int ssn) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gpa = gpa;
+		this.ssn = ssn;
+	}
+
+	@Override
+	public void learn() {
+		System.out.println("Student learns");
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	public int getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", gpa=" + gpa + ", ssn=" + ssn + "]";
+	}
+
+
+	
+	
+}
