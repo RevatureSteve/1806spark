e8b1eb6445552a64449e6f7005107bacdcc75640
richardiskra <richard.iskra@valpo.edu>
2018-08-03 23:34:25 -0400

    bank db read

--- intro-java/BankApp/src/com/revature/bank/BankLauncher.java
+++ intro-java/BankApp/src/com/revature/bank/BankLauncher.java
@@ -1,11 +1,16 @@
 package com.revature.bank;
 
+import com.revature.bank.dao.BankDao;
+import com.revature.bank.pojo.User;
+import com.revature.bank.presentation.Presentation;
+
 public class BankLauncher {
 	public static void main(String[] args) {
-		Screen currentScreen = new MainMenuScreen();
-		System.out.println("Welcome to the bank!");
-		while(true) {
-			currentScreen = currentScreen.start();
-		}
+		System.out.println("Welcome to the bank! please login");
+		BankDao bDao = new BankDao();
+		User u = Presentation.login();
+		System.out.println(u);
+		User db_u = bDao.getFlashCardById(u.getUsername());
+		System.out.println(db_u);
 	}
 }
--- intro-java/BankApp/src/com/revature/bank/ViewBalanceScreen.java
+++
@@ -1,7 +0,0 @@
-package com.revature.bank;
-
-import java.util.Scanner;
-
-public interface ViewBalanceScreen{
-	public double balance = 785.50;
-}
