d5fd463e09e445c3e8ac9723a60b8f28c25f46b7
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-08-05 19:53:35 -0400

    banking app interface added

---
+++ JDBC/BankJdbc/src/com/revature/Driver.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import com.revature.presentation.MainMenu;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println("testing...");
+		
+		MainMenu.welcome();
+	}
+
+}
