0e503187a5a2a733e23354d874574ec6cca46d9d
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-08-05 14:24:20 -0400

    JDBC working

---
+++ JDBC/FlashCardJdbc/src/com/revature/Driver.java
@@ -0,0 +1,79 @@
+package com.revature;
+
+import java.util.Scanner;
+
+import com.revature.dao.FlashCardDao;
+import com.revature.dao.FlashCardDaoImpl;
+import com.revature.pojo.FlashCard; 
+import com.revature.presentation.Menus;
+/*
+ * imported these because of the classes below 
+ * No classes required to be made
+ * Make sure you import the correct class
+ */
+
+
+public class Driver {
+	
+	/*
+	 * JDBC: Java Database Connectivity
+	 * 		JDBC is an API
+	 * 
+	 * JBBC API to connect to RDBMS and not just OracleDB
+	 * 
+	 * We can connect to any RDBMS that has a jar 
+	 * 		available that implements the JDBC API
+	 * 
+	 * JDBC is a set of Interfaces given to each DB 
+	 * 		to implement so we can quickly switch from 
+	 * 		one DB to another if needed w/o much code.
+	 * 
+	 * The important interfaces of JDBC
+	 * 		1. connection (most important) 
+	 * 		2. statement (from the connection) NEVER USE!
+	 * 		3. prepredStatement (from Connection) WILL ALWAYS USE THIS INSTEAD!
+	 * 		4. callableStatement (from Connection)
+	 * 
+	 * 		I require a connection to so one of the others 
+	 * 		at a time.
+	 * 
+	 * 		JAR: Java Archive
+	 * 			Multiple Compiled .class files into one executable		
+	 * 
+	 * 		What is required for a connection?
+	 * 			1. The Driver: the jar that implements JDBC
+	 * 			or that specific DB
+	 * 				-a jar for OravleDB
+	 * 				-a jar for MySQL (DB)
+	 * 				-a jar for SQL Server (DB)
+	 * 				-ect
+	 * 			2. URL to the DB
+	 * 			3. Username
+	 * 			4. Password
+	 * 
+	 * 		Need multiple objects(records) from the database? 
+	 * 		Use a Collection like List<FlashCard> flashcardList = new ArrayList<>(); 
+	 * 
+	 * 		3 Layers in Java Enterprise Architecture
+	 * 			1. Presentation: display data (pojo's current state) to the user and take input
+	 * 			2. Business Logic: modify input (again the pojo), redirect user to proper presentations, call persistance methods when needed
+	 * 			3. Persistance
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+		FlashCardDao fcDao = new FlashCardDaoImpl();
+		
+		Menus.welcome();
+		FlashCard userInputFlashCard = Menus.createFlashCardMenu();
+		System.out.println(userInputFlashCard);
+		
+		fcDao.createFlashcardProc(userInputFlashCard);
+	}
+}
+	
+	
+	
+	
+	
+	
+
---
+++ JDBC/FlashCardJdbc/src/com/revature/dao/FlashCardDaoImpl.java
@@ -0,0 +1,129 @@
+package com.revature.dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.pojo.FlashCard;
+
+public class FlashCardDaoImpl implements FlashCardDao {
+
+	private final static String USERNAME = "flashcard_db";
+	private final static String PASSWORD = "p4ssw0rd";
+	private final static String URL = "jdbc:oracle:thin:@octocat.cxwq5mzpnovv.us-east-2.rds.amazonaws.com:1521:ORCL";
+
+	//create
+	@Override
+	public int createFlashcard(FlashCard fc) {
+		
+		int rowsAffected = 0;
+		
+		try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);) {
+			conn.setAutoCommit(false);
+			
+			String sql = "INSERT INTO flash_card(fc_question, fc_answer) VALUES (?, ?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			ps.setString(1, fc.getFcQuestion());
+			ps.setString(2, fc.getFcAnswer());
+			
+			rowsAffected = ps.executeUpdate();
+			conn.commit();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rowsAffected;
+	}
+	
+	@Override
+	public void createFlashcardProc(FlashCard fc) {
+		try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);) {
+			String sql = "{call insert_fc_procedure(?, ?, ?)}";
+			String msg = "";
+			CallableStatement cs = conn.prepareCall(sql);
+			cs.setString(1, fc.getFcQuestion());
+			cs.setString(2, fc.getFcAnswer());
+			cs.setString(3, msg);
+			int rowsAffected = cs.executeUpdate();
+			System.out.println("rows affected: " + rowsAffected);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//read
+	@Override
+	public List<FlashCard> getAllFlashCards() {
+
+		/*
+		 * flashcard_db p4ssw0rd octocat.cxwq5mzpnovv.us-east-2.rds.amazonaws.com 1521
+		 * ORCL
+		 */
+		/*
+		 * String username = "flashcard_db"; String password = "p4ssw0rd"; String url =
+		 * "jdbc:oracle:thin:@octocat.cxwq5mzpnovv.us-east-2.rds.amazonaws.com:1521:ORCL";
+		 */
+
+		// get a connection to the OracleDB
+		// Use the DriverManager
+
+		// make a list for the flashcards
+		List<FlashCard> myFlashCards = new ArrayList<>();
+
+		try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);) {
+			String sql = "SELECT * FROM Flash_cards";
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+
+			System.out.println(rs);
+			// loop through the resultSet of flashcards records
+			while (rs.next()) {
+				/*
+				 * getInt(colummn Index) or getInt(colummn label)
+				 */
+				/*
+				 * System.out.println("ID: " + rs.getInt(1) + " Question: " +
+				 * rs.getString("fc_question") + " Answer: " + rs.getString("fc_answer"));
+				 */
+				FlashCard fc = new FlashCard(rs.getInt("fc_id"), rs.getString(2), rs.getString("fc_answer"));
+				// System.out.println(fc);
+				myFlashCards.add(fc);
+			}
+
+			/*
+			 * In the event you get an error called "No Suitable Driver" you must add the
+			 * jar
+			 */
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return myFlashCards;// get me the flashcards
+	}
+
+	public FlashCard getFlashCardById(int id) {
+		FlashCard fc = null;
+		try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);) {
+			String sql = "SELECT * FROM flash_card WHERE fc_id = " + id;
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+
+			if (rs.next()) {
+				fc = new FlashCard(rs.getInt("fc_id"), rs.getString(2), rs.getString("fc_answer"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return fc;
+	}
+
+
+}
