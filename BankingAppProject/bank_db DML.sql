4b650777aae3e14c47388adad6a6c3641c6b01fd
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-08-06 21:28:15 -0400

    commiting sql dml for project

--- BankingAppProject/bank_db DML.sql
+++ BankingAppProject/bank_db DML.sql
@@ -6,6 +6,7 @@
 INSERT INTO USERS VALUES (1, 'loti', 'bananas', 'lo', 'ti');
 INSERT INTO USERS VALUES (2, 'Roti', 'bananas', 'Ro', 'ti');
 INSERT INTO USERS VALUES (3, 'Dude', 'hello', 'man', 'hi');
+INSERT INTO USERS VALUES (4, 'The Dude', 'hey man', 'The Dude', 'Lebowski');
 commit;
 
 select * from users;
@@ -14,6 +15,7 @@ select * from users;
 INSERT INTO bank_account VALUES (12345678, 1000, 1);
 INSERT INTO bank_account VALUES (2, 100, 2);
 INSERT INTO bank_account VALUES (3, 2000, 3);
+INSERT INTO bank_account VALUES (4, 0, 4);
 commit;
 
 select * from bank_account;
@@ -45,11 +47,11 @@ COMMIT;
 END;
 /
 
+-- Anonymous function
 BEGIN
-    deposit_into_account(1000, 2);
+    deposit_into_account(1000.25, 3);
 END;
 /
-
 
 CREATE OR REPLACE PROCEDURE withdraw_from_account(amt INT, accNum INT)
 AS
@@ -66,6 +68,28 @@ BEGIN
 END;
 
 
+CREATE OR REPLACE PROCEDURE create_user_and_bank_account
+( un IN VARCHAR2, pw IN VARCHAR2, fname IN VARCHAR2, lname IN VARCHAR2)
+IS
+BEGIN
+INSERT INTO users(username, password, fname, lname) VALUES(un, pw, fname, lname);
+INSERT INTO bank_account(balance, users_id) VALUES (0,(SELECT users_id FROM users WHERE un = users.username));
+COMMIT;
+END;
+/
+
+BEGIN
+create_user_and_bank_account('pichu', '***', 'Ash', 'Ketchum');
+END;
+/
+
+SELECT * FROM USERS u
+INNER JOIN bank_account b ON u.users_id = b.users_id;
+
+
+
+
+-- Get all transaction history for one 
 CREATE OR REPLACE PROCEDURE get_transaction_history_proc(acc_num IN NUMBER, cursorParam OUT SYS_REFCURSOR)
 IS 
 BEGIN
@@ -100,7 +124,8 @@ END;
 SELECT tx.tx_timestamp, tx.tx_amt, t.tx_type, tx.account_number
     FROM bank_tx tx
     INNER JOIN bank_tx_type t ON tx.tx_type_id = t.tx_type_id
-    WHERE tx.account_number = 2;
+    WHERE tx.account_number = 12345678
+    ORDER BY Convert(datetime,tx.tx_timestamp,100) DESC;
 
 
 -- Sequence statements
@@ -109,19 +134,48 @@ CREATE SEQUENCE tx_id_seq
   INCREMENT BY 1;
 /
 
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1;
+/
+
+CREATE SEQUENCE bank_account_seq
+    START WITH 1
+    INCREMENT BY 1;
+/
 
 --TRIGGER FOR ID INCREMENT
 CREATE OR REPLACE TRIGGER tx_id_seq_trigger
 BEFORE INSERT ON bank_tx
-FOR EACH ROW  --required if you want to see/manipulate the row's data
+FOR EACH ROW 
 BEGIN
---allowed to write PL/SQL
   IF :new.tx_id IS NULL THEN
-    --embedded SQL
     SELECT tx_id_seq.nextval INTO :new.tx_id FROM DUAL;
-    --DUAL is a dummy table, typically used when you don't require a table 
-    --but you still need the syntax of a sql statement to be correct
   END IF;
 END;
 /
+
+CREATE OR REPLACE TRIGGER user_id_seq_trigger
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF :new.users_id IS NULL THEN
+      SELECT USERS_ID_SEQ.nextval INTO :new.users_id FROM DUAL;
+    END IF;
+END;
+/
+
+
+DROP TRIGGER bank_account_seq_trigger;
+CREATE OR REPLACE TRIGGER bank_account_seq_trigger
+BEFORE INSERT ON bank_account
+FOR EACH ROW 
+BEGIN   
+    IF :new.account_number IS NULL THEN
+      SELECT BANK_ACCOUNT_SEQ.nextval INTO :new.account_number FROM DUAL;
+    END IF;
+END;
+/
+    
+
 
