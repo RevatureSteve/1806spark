5e31f05de5f4ff168eb6dc27537aa456c0ba6012
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-08-09 21:51:15 -0400

    sql dml tables added

--- daniel_sidhu_code/Project-1/Project_1_DML.sql
+++ daniel_sidhu_code/Project-1/Project_1_DML.sql
@@ -1,12 +1,13 @@
 CREATE TABLE users 
 (
 
-    U_Id,
-    Email,
-    Password,
-    fname,
-    lname
-    Pos_Id
+    U_Id NUMBER,
+    Email VARCHAR2(4000) UNIQUE NOT NULL,
+    Password VARCHAR(4000)NOT NULL,
+    fname VARCHAR(4000) NOT NULL,
+    lname VARCHAR(4000) NOT NULL,
+    Pos_Id NUMBER NOT NULL,
+    PRIMARY KEY (U_Id)
 
 );
 
@@ -14,15 +15,54 @@ CREATE TABLE users
 CREATE TABLE position
 (
 
-    Pos_Id
-    Pos_Type
-
+    Pos_Id NUMBER,
+    Pos_Type NUMBER UNIQUE NOT NULL,
+    PRIMARY KEY (Pos_Id), 
+    FOREIGN KEY (Pos_Id) REFERENCES users(Pos_Id)
+    
 );
 
 
 CREATE TABLE reimbursement 
 (
 
+    R_Id NUMBER,
+    Emp_U_Id NUMBER NOT NULL,
+    Mgr_U_Id NUMBER NOT NULL,
+    Amt NOT NULL,
+    Description,
+    Img BLOB,
+    Timesubmission,
+    Rq_Type_Id,
+    Rq_Status_Id,
+    CONSTRAINT PK_reimbursement PRIMARY KEY (R_Id),
+    FOREIGN KEY (Emp_U_Id) REFERENCES users(U_Id),
+    FOREIGN KEY (Mgr_U_Id) REFERENCES users(U_Id)
     
+);
+
+
+CREATE TABLE rq_type
+(
+
+    Rq_Type_Id,
+    Rq_Type,
+    CONSTRAINT PK_rq_Type PRIMARY KEY (Rq_Type_Id)
 
 );
+
+
+CREATE TABLE rq_status
+(
+
+    Rq_Status_Id,
+    Rq_Status,
+    CONSTRAINT PK_rq_type PRIMARY KEY (Rq_Status_Id)
+
+);
+
+
+
+
+
+
