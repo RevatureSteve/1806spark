a7fe5a51a005e38eb94a9c60d8333fd0dd8f0fa8
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-08-16 20:00:11 -0400

    servlet examples

---
+++ rhys_yamasaki_code/servlet/src/main/java/com/revature/WebDriver.java
@@ -0,0 +1,22 @@
+package com.revature;
+
+public class WebDriver {
+
+	/*
+	 *  Comment for a Maven WAR project (web archive)
+	 * 
+	 * 	No longer using a main method at all!
+	 * 	Tomcat is the entry point (had the main method)
+	 * 		We provide Servlets to Tomcat to provide 
+	 * 		logic on how to handle requests and send back 
+	 * 		Responses
+	 * 
+	 * 	The 2 first things after creating a Maven War project
+	 * 		- update java version to 1.8 in pom.xml
+	 * 		- generate the web.xml aka deployment descriptor
+	 * 			(right-click on project -> java EE tools
+	 * 				-> generate deployment descriptor stub)
+	 *
+	 * 
+	 */
+}
