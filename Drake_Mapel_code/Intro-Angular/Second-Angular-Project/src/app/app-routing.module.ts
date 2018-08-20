1f91e9e690fc1c93d12fdf7b49e52a402e73fab9
Drake_Mapel <40637038+dmapel@users.noreply.github.com>
2018-08-20 11:53:07 -0400

    angular

---
+++ Drake_Mapel_code/Intro-Angular/Second-Angular-Project/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { MainComponent } from "./main/main.component";
+import { ClickerComponent } from "./clicker/clicker.component";
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home'},
+    {path: 'home', component: MainComponent},
+    {path: 'dashboard', component: ClickerComponent,},
+    {path: '**', redirectTo: 'home'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
--- Drake_Mapel_code/Intro-Angular/Second-Angular-Project/src/app/app.module.ts
+++ Drake_Mapel_code/Intro-Angular/Second-Angular-Project/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClickerComponent } from './clicker/clicker.component';
 import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClickerComponent,
-    NavComponent
+    NavComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
