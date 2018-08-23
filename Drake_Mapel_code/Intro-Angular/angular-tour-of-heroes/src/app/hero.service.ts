0956f5b5e09067a3c51fb301e6ea3fcd23f9ed68
Drake_Mapel <40637038+dmapel@users.noreply.github.com>
2018-08-22 21:30:25 -0400

    vs code weirdness

--- Drake_Mapel_code/Intro-Angular/angular-tour-of-heroes/src/app/hero.service.ts
+++ Drake_Mapel_code/Intro-Angular/angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
