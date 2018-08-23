69380ec6234dd1e786f17d70c29c1aa4c42e08fe
cholmes1991 <holmesc@mail.usf.edu>
2018-08-23 13:37:47 -0400

    class push

---
+++ chris_holmes_code/Intro-Angular/Ers-Project/Ers-Project/src/app/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+ /*  getUsers{
+    return this.http.
+    public get value() : string {
+      return
+    }  */
+
+}
