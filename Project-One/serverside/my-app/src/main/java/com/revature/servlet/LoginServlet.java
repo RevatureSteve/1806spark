7c9cde347ac203ad3ea439d72ebc3fe1163d39f2
Beck Larson - Work <becklarson-Work@107-144-156-227.biz.bhn.net>
2018-08-23 22:08:35 -0400

    backing up

--- Project-One/serverside/my-app/src/main/java/com/revature/servlet/LoginServlet.java
+++ Project-One/serverside/my-app/src/main/java/com/revature/servlet/LoginServlet.java
@@ -1,10 +1,17 @@
 package com.revature.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.revature.logic.UserLogic;
+import com.revature.pojo.TranstarUsers;
 
 /**
  * Servlet implementation class LoginServlet
@@ -23,16 +30,43 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String email = null;
+		String password = null;
+		
+		System.out.println("POST- Login");
+		TranstarUsers user = new TranstarUsers();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		ServletInputStream body = request.getInputStream();
+		ObjectNode n = mapper.readValue(body, ObjectNode.class);
+		
+		if(n.has("email")) {
+			email = n.get("email").textValue();
+		}
+		
+		if(n.has("password")) {
+			password = n.get("password").textValue();
+		}
+		System.out.println(email + " " + password);
+		user = new UserLogic().login(email, password);
+		
+		
+		
+		
+		String json = mapper.writeValueAsString(user);
+		
+		response.setContentType("application/json");
+		response.getWriter().write(json);
 	}
+	
+
 
 }
