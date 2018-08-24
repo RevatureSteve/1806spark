ee0a6422808dfb404e78d070f8055beafa37b396
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-08-23 22:51:37 -0400

    project 1 client router works

---
+++ daniel_sidhu_code/Project-1-Angular/ERS-Angular/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- daniel_sidhu_code/Project-1-Angular/ERS-Angular/src/app/app.module.ts
+++ daniel_sidhu_code/Project-1-Angular/ERS-Angular/src/app/app.module.ts
@@ -1,14 +1,28 @@
+/*
+  When I generate a component, double check this page!!
+  This page is resposible for all the routing to different pages (i.e. home, login, ect.)
+  each component should have an import and an export
+*/
+
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
