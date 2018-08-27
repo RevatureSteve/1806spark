cd922bc4e322aff6afabab455dead2ad44101c79
AlexMoraga <alex.moraga94@gmail.com>
2018-08-27 07:20:41 -0400

    Update case tested and complete

--- Project_One/Angular/Project-One/src/app/pages/reimbursement/reimbursement.component.ts
+++ Project_One/Angular/Project-One/src/app/pages/reimbursement/reimbursement.component.ts
@@ -1,3 +1,5 @@
+import { LoggedInService } from './../../logged-in.service';
+import { Users } from './../../models/Users';
 import { Reimbursement } from './../../models/Reimbursement';
 import { DataService } from './../../data.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,11 +12,13 @@ import { Observable } from 'rxjs';
 })
 export class ReimbursementComponent implements OnInit {
   postR: Reimbursement[];
+  user: Users;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, private loggedInService: LoggedInService) { }
 
   ngOnInit() {
     this.getReimb();
+    this.user = this.loggedInService.getLoggedInUser();
   }
 
   getReimb() {
