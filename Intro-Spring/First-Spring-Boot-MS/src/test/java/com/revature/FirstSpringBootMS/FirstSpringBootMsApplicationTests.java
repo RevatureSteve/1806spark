b1e4f03f977e3b5b0637540f3a4848ebe615b4f7
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-08-31 19:42:57 -0400

    steve's spring folder pulled

---
+++ Intro-Spring/First-Spring-Boot-MS/src/test/java/com/revature/FirstSpringBootMS/FirstSpringBootMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revature.FirstSpringBootMS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstSpringBootMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
