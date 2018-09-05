20bacf5e1193eb9d7bd66c808019238e8e5a3094
Eddie Grays <eddiegrays10@gmail.com>
2018-09-04 23:01:03 -0400

    need to switch branches

---
+++ Intro_Spring/Spring-Todo-App/src/main/java/com/revature/SpringTodoApp/SpringTodoAppApplication.java
@@ -0,0 +1,12 @@
+package com.revature.SpringTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoAppApplication.class, args);
+	}
+}
