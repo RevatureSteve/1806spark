2983bf51d14fb93632c3264f5abf2803ff65cc4c
MarcinSalamon <Marcin+salamonmarcin29@gmail.com>
2018-09-07 10:08:02 -0400

    git keeper

---
+++ Intro-Spring/HelloFrontController/src/main/java/com/revature/controller/RequestHelper.java
@@ -0,0 +1,9 @@
+package com.revature.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestHelper {
+	public static String process(HttpServletRequest req) {
+		return "index.html";
+	}
+}
---
+++ Intro-Spring/HelloFrontController/src/main/java/com/revature/servlet/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.revature.servlet;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4439637135600892326L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+	}
+	
+}
